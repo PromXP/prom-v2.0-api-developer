3650788a1a5f967097271daa97980e874d541291
Inbasekar <inbatech2024@gmail.com>
2025-08-28 19:25:41 +0530

    Initial commit

--- app.py
+++ app.py
@@ -15,8 +15,6 @@ import os
 # Load variables from .env file
 load_dotenv()
 
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 
 
 
@@ -336,8 +334,8 @@ async def assign_questionnaire_bulk(data: list[QuestionnaireAssignment]):  # Acc
 
 s3 = boto3.client(
     "s3",
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    aws_access_key_id="AKIAQQ5O6E7YROLOVJGG",
+    aws_secret_access_key="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",
     region_name="us-west-2",
 )
 
